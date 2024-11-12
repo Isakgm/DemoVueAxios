de928550dd51f1b8cc2e9d6b2002dda891112f9e
Isakgm <Isakgm@gmail.com>
2024-11-12 14:11:34 +0100

    Virker med API med getAll

--- js/main.js
+++ js/main.js
@@ -1,17 +1,43 @@
+const baseurl = "https://actorone.azurewebsites.net/api/actor"
+
 const app = Vue.createApp({ 
     data() { 
         return {
              intro: 'Welcome to my Vue template',
+             actorList:[],
+             actor: null,
+
              } 
             }, 
             methods: {
                  myMethod(){
-
+                    
                   }, 
+                  getAllActors(){
+                    console.log("Er i metoden getAllActors");
+
+                    axios.get(baseurl)
+                    .then(
+                        response => {
+                            this.actorList = response.data
+                            console.log(response)
+                            
+                        }
+                    )
+                    .catch(
+                        error => {
+                            console.log(error)
+                        }
+                    )
+                  },
+                  getById(){
+                    
+                  }
                 }, 
                 computed: { 
                     myComputed() { 
                         return ''
                      }, 
                     } 
-                })
+                });
+
